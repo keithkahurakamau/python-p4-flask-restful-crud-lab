5398f27538c09f4ce96e5a2107e97ba23a5c2df1
keithkahurakamau <keithkahura@gmail.com>
2025-11-30 18:06:54 +0300

    restful-crud

--- server/app.py
+++ server/app.py
@@ -47,9 +47,37 @@ class PlantByID(Resource):
         plant = Plant.query.filter_by(id=id).first().to_dict()
         return make_response(jsonify(plant), 200)
 
+    def patch(self, id):
+        plant = Plant.query.filter_by(id=id).first()
+        
+        # Guard clause in case plant does not exist (optional but recommended)
+        if not plant:
+            return make_response(jsonify({'error': 'Plant not found'}), 404)
+
+        data = request.get_json()
+
+        # Iterate through the request data and update corresponding attributes
+        for attr in data:
+            setattr(plant, attr, data[attr])
+
+        db.session.add(plant)
+        db.session.commit()
+
+        return make_response(jsonify(plant.to_dict()), 200)
+
+    def delete(self, id):
+        plant = Plant.query.filter_by(id=id).first()
+        
+        # Guard clause in case plant does not exist
+        if not plant:
+            return make_response(jsonify({'error': 'Plant not found'}), 404)
+
+        db.session.delete(plant)
+        db.session.commit()
+
+        return make_response('', 204)
 
 api.add_resource(PlantByID, '/plants/<int:id>')
-
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
